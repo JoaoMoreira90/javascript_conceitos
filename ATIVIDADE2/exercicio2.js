0caffdb599e757f87df1e3d86de7de09bd72d91d
joão <oivictor446@gmail.com>
2026-09-22 09:09:33 -0300

    Add files via upload

---
+++ ATIVIDADE2/exercicio2.js
@@ -0,0 +1,10 @@
+//Uma maquina deve ser clasificda de acordo com a temperatura: ate 60 c = normal; de 61 ate 80 c = atencao; acima de 80 c = critica
+
+const entrada = require('readline-sync');
+const temperatura = entrada.questionFloat("digite a temperatuta: ")
+
+let situacao;
+
+if (temperatura <=60) {
+    
+}
